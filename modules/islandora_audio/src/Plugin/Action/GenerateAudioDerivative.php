e11363b0cfbaf9b9767dad1814dcc1d77f1ba1c7
Natkeeran <natkeeran@gmail.com>
2019-02-08 10:26:52 -0500

    islandora_audio plugin module (#114)
    * islandora_audio plugin module
    
    * changes requested by Danny

---
+++ modules/islandora_audio/src/Plugin/Action/GenerateAudioDerivative.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\islandora_audio\Plugin\Action;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\islandora\Plugin\Action\AbstractGenerateDerivative;
+
+/**
+ * Emits a Node for generating audio derivatives event.
+ *
+ * @Action(
+ *   id = "generate_audio_derivative",
+ *   label = @Translation("Generate a audio derivative"),
+ *   type = "node"
+ * )
+ */
+class GenerateAudioDerivative extends AbstractGenerateDerivative {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $config = parent::defaultConfiguration();
+    $config['path'] = '[date:custom:Y]-[date:custom:m]/[node:nid].mp3';
+    $config['mimetype'] = 'audio/mpeg';
+    $config['queue'] = 'islandora-connector-homarus';
+    $config['destination_media_type'] = 'audio';
+    $config['args'] = '-codec:a libmp3lame -q:a 5';
+    return $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['mimetype']['#description'] = t('Mimetype to convert to (e.g. audio/mpeg, audio/m4a, etc...)');
+    $form['args']['#description'] = t('Additional command line parameters for FFMpeg');
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::validateConfigurationForm($form, $form_state);
+    $exploded_mime = explode('/', $form_state->getValue('mimetype'));
+    if ($exploded_mime[0] != 'audio') {
+      $form_state->setErrorByName(
+        'mimetype',
+        t('Please enter a audio mimetype (e.g. audio/mpeg, audio/m4a, etc...)')
+      );
+    }
+  }
+
+}
---
+++ modules/islandora_audio/tests/src/Functional/GenerateAudioDerivativeTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\Tests\islandora_audio\Functional;
+
+use Drupal\Tests\islandora\Functional\GenerateDerivativeTestBase;
+
+/**
+ * Tests the GenerateAudioDerivative action.
+ *
+ * @group islandora_audio
+ */
+class GenerateAudioDerivativeTest extends GenerateDerivativeTestBase {
+
+  protected static $modules = ['context_ui', 'islandora_audio'];
+
+  /**
+   * @covers \Drupal\islandora_audio\Plugin\Action\GenerateAudioDerivative::defaultConfiguration
+   * @covers \Drupal\islandora_audio\Plugin\Action\GenerateAudioDerivative::buildConfigurationForm
+   * @covers \Drupal\islandora\Plugin\Action\AbstractGenerateDerivative::defaultConfiguration
+   * @covers \Drupal\islandora\Plugin\Action\AbstractGenerateDerivative::buildConfigurationForm
+   * @covers \Drupal\islandora\Plugin\Action\AbstractGenerateDerivative::submitConfigurationForm
+   * @covers \Drupal\islandora\Plugin\Action\AbstractGenerateDerivative::execute
+   */
+  public function testGenerateAudioDerivativeFromScratch() {
+
+    // Create a test user.
+    $account = $this->drupalCreateUser([
+      'bypass node access',
+      'administer contexts',
+      'administer actions',
+      'view media',
+      'create media',
+      'update media',
+    ]);
+    $this->drupalLogin($account);
+
+    // Create an action to generate a audio derivative.
+    $this->drupalGet('admin/config/system/actions');
+    $this->getSession()->getPage()->findById("edit-action")->selectOption("Generate a audio derivative");
+    $this->getSession()->getPage()->pressButton(t('Create'));
+    $this->assertSession()->statusCodeEquals(200);
+
+    $this->getSession()->getPage()->fillField('edit-label', "Generate audio test derivative");
+    $this->getSession()->getPage()->fillField('edit-id', "generate_audio_test_derivative");
+    $this->getSession()->getPage()->fillField('edit-queue', "generate-audio-test-derivative");
+    $this->getSession()->getPage()->fillField("edit-source-term", $this->preservationMasterTerm->label());
+    $this->getSession()->getPage()->fillField("edit-derivative-term", $this->serviceFileTerm->label());
+    $this->getSession()->getPage()->fillField('edit-mimetype', "audio/mpeg");
+    $this->getSession()->getPage()->fillField('edit-args', "-f mp3");
+    $this->getSession()->getPage()->fillField('edit-scheme', "public");
+    $this->getSession()->getPage()->fillField('edit-path', "derp.mov");
+    $this->getSession()->getPage()->pressButton(t('Save'));
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Create a context and add the action as a derivative reaction.
+    $this->createContext('Test', 'test');
+    $this->addPresetReaction('test', 'derivative', "generate_audio_test_derivative");
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Create a new preservation master belonging to the node.
+    $values = [
+      'name[0][value]' => 'Test Media',
+      'files[field_media_file_0]' => __DIR__ . '/../../fixtures/test_file.txt',
+      'field_media_of[0][target_id]' => 'Test Node',
+      'field_tags[0][target_id]' => 'Preservation Master',
+    ];
+    $this->drupalPostForm('media/add/' . $this->testMediaType->id(), $values, t('Save'));
+
+    $expected = [
+      'source_uri' => 'test_file.txt',
+      'destination_uri' => "node/1/media/{$this->testMediaType->id()}/3",
+      'file_upload_uri' => 'public://derp.mov',
+      'mimetype' => 'audio/mpeg',
+      'args' => '-f mp3',
+      'queue' => 'islandora-connector-homarus',
+    ];
+
+    // Check the message gets published and is of the right shape.
+    $this->checkMessage($expected);
+  }
+
+}
