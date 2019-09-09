c3fa84500ea716c61c667491dd6f1cebbeefff09
Jared Whiklo <jwhiklo@gmail.com>
2019-09-09 09:18:43 -0500

    Hide media and members tabs on non-Islandora objects. (#160)
    * Use custom access to test type of node
    
    * Also hide Members tab
    
    * Separate permission for node fields
    
    * Check for correct field
    
    * Add phpdoc and make both fields required.
    
    * Remove whitespace

--- src/Controller/ManageMediaController.php
+++ src/Controller/ManageMediaController.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\islandora\Controller;
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
 
 /**
@@ -14,6 +17,9 @@ class ManageMediaController extends ManageMembersController {
    *
    * @param \Drupal\node\NodeInterface $node
    *   Node you want to add a media to.
+   *
+   * @return array
+   *   Array of media types to add.
    */
   public function addToNodePage(NodeInterface $node) {
     return $this->generateTypeList(
@@ -24,6 +30,28 @@ class ManageMediaController extends ManageMembersController {
       $node,
       'field_media_of'
     );
+  }
+
+  /**
+   * Check if the object being displayed "is Islandora".
+   *
+   * @param \Drupal\Core\Routing\RouteMatch $route_match
+   *   The current routing match.
+   *
+   * @return \Drupal\Core\Access\AccessResultAllowed|\Drupal\Core\Access\AccessResultForbidden
+   *   Whether we can or can't show the "thing".
+   */
+  public function access(RouteMatch $route_match) {
+    if ($route_match->getParameters()->has('node')) {
+      $node = $route_match->getParameter('node');
+      if (!$node instanceof NodeInterface) {
+        $node = Node::load($node);
+      }
+      if ($node->hasField('field_model') && $node->hasField('field_member_of')) {
+        return AccessResult::allowed();
+      }
+    }
+    return AccessResult::forbidden();
   }
 
 }
